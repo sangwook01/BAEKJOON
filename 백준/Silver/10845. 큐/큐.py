1474e51143221d5bc5f43f39125b3323ab73ae16
Sangwook KIM <133327420+sangwook01@users.noreply.github.com>
2024-03-14 23:17:20 +0900

    Update 큐.py

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -37,3 +37,47 @@ for i in range(num):
         else:
             print(q.pop(0))
 
+## append랑 pop을 쓰지 않고 하는 또다른 방법
+
+num = int(input())
+
+q = []
+
+
+for i in range(num):
+    act = input().split()
+    if act[0] == 'push':
+        qq = [int(act[1])]
+        qq[:-1] = q
+        q = qq
+        
+        temp = act[1]
+
+    elif act[0] == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    
+    elif act[0] == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
+    
+    elif act[0] == 'size':
+        print(len(q))
+        
+    elif act[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif act[0] == 'pop':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+            q = q[1:]
+
