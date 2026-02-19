c9cc9df1391e845c6bdd1d5d9d03ee64e2fbb053
Sangwook KIM <133327420+sangwook01@users.noreply.github.com>
2026-02-19 16:14:45 +0900

    [Gold V] Title: 집합의 표현, Time: 348 ms, Memory: 71968 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,58 @@
+'''
+합집합을 의미하는 것은 0 -> union_parent
+같은 집합에 포함되는지 확인하는 것 1 -> find_parent => 같은 집합에 속하느냐? == a,b가 같은 부모 아래 있는가?
+N의 값이 1,000,000까지로 범위가 매우 넓음.... 
+일반적인 서로소 집합 자료 구조를 활용하기엔 무리가 있음.. => 런타임 에러 발생
+1. 이때 바꿀 수 있는 것 (재귀적 함수 호출을 while문으로 변경...)
+2. 재귀 호출의 횟수 늘리기 (이건 코딩테스트에 있어서 별로 좋지 않은 구조라 생각)
+'''
+
+import sys
+input = sys.stdin.readline
+'''
+해당 함수를 재귀가 아닌 while문으로 변경 
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+'''
+def find_parent(parent, x):
+    root = x
+    while parent[root] != root:
+        root = parent[root]
+
+    # 경로 압축
+    while parent[x] != x:
+        next_x = parent[x]
+        parent[x] = root
+        x = next_x
+
+    return root
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)    
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+        
+n,m = map(int, input().split())
+
+parent = [0]*(n+1)
+for i in range(0,n+1):
+    parent[i] = i
+
+# 0,1에 따라서 각 함수를 다르게 수행해야됨
+for i in range(m):
+    oper, a, b = map(int,input().split())
+    if oper == 0:
+        union_parent(parent, a, b)
+        
+    elif oper == 1:
+        if find_parent(parent,a) == find_parent(parent, b) :
+            print("YES")
+        else:
+            print("NO")
