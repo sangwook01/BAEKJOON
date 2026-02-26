b46af5b1ca1a23e5269391e13ca37d0584e7b57f
Sangwook KIM <133327420+sangwook01@users.noreply.github.com>
2026-02-26 15:47:29 +0900

    [Silver V] Title: 단어 정렬, Time: 80 ms, Memory: 37024 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,28 @@
+'''
+1. 길이가 짧은 순서대로
+2. 길이가 같다면 사전 순서대로
+3. 마지막에 set으로 중복제거하기
+<생각>
+문자열을 정렬하는 과정에서는 tuple 형태를 활용하면 보다 편하게 구현할 수 있음
+(a,b)-> 왼쪽부터 차례대로 조건에 맞게 바꿈
+<과정>
+3번 과정 먼저 수행
+1,2 순서로 sort에 대해 lambda 함수 구현하기
+
+'''
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+lst = []
+for _ in range(n):
+    lst.append(input().strip())
+
+# 중복제거    
+lst = list(set(lst))
+
+lst.sort(key = lambda x: (len(x),x))    
+ 
+for c in lst:
+    print(c)
